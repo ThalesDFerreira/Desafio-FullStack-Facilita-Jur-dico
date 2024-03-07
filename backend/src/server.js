cabaeb412f5ffdadc8c95fed0d634599c1407591
ThalesDFerreira <thalesferreira190@gmail.com>
2024-03-07 13:44:30 -0300

    feat: montando o server.js

---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT;
+
+// const routes = require('./routes/routes');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// app.use(routes);
+
+app.listen(port, () => console.log(`Api rodando na porta ${port}`));
